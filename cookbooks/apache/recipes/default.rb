c8fbbb2ad2b6a7391972e1f39439b3c903a07411
Ian Godfrey <igodfrey92@gmail.com>
2018-05-15 01:47:23 +0000

    created motd file and edited default.rb

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,14 +4,24 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+#include the websites recipe
+#include_recipe 'apache::websites'
+
+#set package varibale to correct package name based on platform family
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == 'debian'
+	package = "apache2"
+end
+
 #install apache package
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
 #start and enable apache service
-sevice 'apache2' do
+service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
